53dc8db0a75e3bd05e8185faa125f8cb3189ffad
barkinspmz <barkinsapmaz2@outlook.com>
2023-12-19 16:45:00 +0300

    Question script has been initiated.
    The base structure of Question. It is custom class that will shape the form of question. It will be used by Singleton Monobehavior script named QuestionController.

---
+++ LingomaticDev/Assets/Scripts/Question.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Question 
+{
+    public string question;
+    public string answerLeft;
+    public string answerRight;
+}
